5060b4f86fdca49c1bc035d27cc064bfdc0f62ea
lotus64yt <lotus64santon@gmail.com>
2026-06-08 04:33:52 +0200

    feat: add Button component

---
+++ components/button.go
@@ -0,0 +1,42 @@
+package components
+
+import (
+	"fmt"
+
+	menu "github.com/octarahq/goclic"
+)
+
+type Button struct {
+	label   string
+	onClick func()
+}
+
+func NewButton(label string, onClick func()) *Button {
+	return &Button{
+		label:   label,
+		onClick: onClick,
+	}
+}
+
+func (d *Button) GetName() string {
+	return "button"
+}
+
+func (d *Button) IsSelectable() bool {
+	return true
+}
+
+func (l *Button) Render(focused bool) string {
+	if focused {
+		return fmt.Sprintf("> [%s]", l.label)
+	}
+	return fmt.Sprintf("  [%s]", l.label)
+}
+
+func (l *Button) HandleInput(key []byte) bool {
+	if len(key) == 1 && (key[0] == menu.KeyEnter || key[0] == menu.KeySpace) {
+		l.onClick()
+		return true
+	}
+	return false
+}
